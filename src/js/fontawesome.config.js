d381dbb09d25230a31b059891608af582eb7015d
Yaroslav-BV <yavabanote2@gmail.com>
2020-01-03 15:24:56 +0300

    Layout Design c. 0.0.1

--- src/js/fontawesome.config.js
+++ src/js/fontawesome.config.js
@@ -2,9 +2,9 @@ import { library, dom, config } from '@fortawesome/fontawesome-svg-core';
 
 config.autoReplaceSvg = 'nest';
 
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheck, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faSquare, faMinusSquare } from '@fortawesome/free-regular-svg-icons';
+// import { fab } from '@fortawesome/free-brands-svg-icons';
 
-library.add(fas, far, fab);
+library.add(faSquare, faCheck, faUserCircle, faMinusSquare);
 dom.watch();
