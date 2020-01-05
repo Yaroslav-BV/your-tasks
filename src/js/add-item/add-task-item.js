aa61f48683419ef51292e67d69ad2a15803fe24a
Yaroslav-BV <yavabanote2@gmail.com>
2020-01-05 17:20:19 +0300

    Added ability to select and delete task item v.4

--- src/js/add-item/add-task-item.js
+++ src/js/add-item/add-task-item.js
@@ -57,8 +57,8 @@ function addTask() {
     const head = new TaskElem(Consts.head.tag, Consts.head.class).makeElem(),
           number = new NumberTask(Consts.number.tag, Consts.number.class, nummTask).makeElem(),
           date = new DateTask(Consts.date.tag, Consts.date.class).makeElem(),
-          nav = new NavTask(Consts.nav.tag, Consts.nav.class, 'fas fa-check', 'fas fa-minus').makeElem();
-    
+          nav = new NavTask(Consts.nav.tag, Consts.nav.class, 'fas fa-check', 'fas fa-minus').makeElem();      
+
     const content = new TaskElem(Consts.content.tag, Consts.content.class).makeElem(),
           textTask = new TextTask(Consts.textTask.tag, Consts.textTask.class, textInput).makeElem();
 
