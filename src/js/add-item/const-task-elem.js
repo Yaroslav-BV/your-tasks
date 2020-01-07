7c346a84a47a8d954dae4f488a5ab3be175f6dbf
Yaroslav-BV <yavabanote2@gmail.com>
2020-01-07 23:56:14 +0300

    Completed the task counter in order, added a color constant v.5

--- src/js/add-item/const-task-elem.js
+++ src/js/add-item/const-task-elem.js
@@ -5,7 +5,15 @@ const ITEM_WRAP = { tag: 'div', class: 'task-list-item' },
       date = { tag: 'span', class: `${ITEM_HEAD.class}-date` },
       nav = { tag: 'span', class: `${ITEM_HEAD.class}-nav` },
       ITEM_CONTENT = { tag: 'div', class: `${ITEM_WRAP.class}-content` },
-      textTask = { tag: 'p', class: `${ITEM_CONTENT.class}-text` };
+      textTask = { tag: 'p', class: `${ITEM_CONTENT.class}-text` },
+      color = {
+        bad: '#ff0000',
+        good: '#32cd32',
+        main: '#fff',
+        itemShadow: '#0000cd',
+        date: '#00008b',
+        unActive: '#b19d9d7a'
+      }
 
 export default {
     wrap: ITEM_WRAP,
@@ -14,5 +22,6 @@ export default {
     date,
     nav,
     content: ITEM_CONTENT,
-    textTask
+    textTask,
+    color
 }
